8a415b52e0c730853b4dada668750718a059558a
IanAdamPrice <ianadamprice@gmail.com>
2022-01-31 15:10:14 -0500

    got view fucntions working

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const connection = mysql.createConnection({
 
 connection.connect(err => {
     if (err) throw err;
-    console.log('connected.');
+    console.log('connected');    
     startQuestions();
 });
 
@@ -33,48 +33,82 @@ const startQuestions = () => {
                 'Update an employee role']
        }
     ])
-    .then(response => {
-      switch(response.action) {
-          case 'View all departments':
-              viewAll('DEPARTMENTS');
-              break;
-          case 'View all roles':
-              viewAll('ROLES');
-              break;
-          case 'View all employees':
-              viewAll('EMPLOYEES');
-              break;
-          case 'Add a department':
-              addDepartment();
-              break;
-          case 'Add a role':
-              addRole();
-              break;
-          case 'Add an employee':
-              addEmployee();
-              break;
-          case 'Update an employee role':
-              updateEmployee();
-              break; 
-          default:
-              Connection.end();         
+    .then((choice) => {
+      const { choices } =  choice;
+      
+      if (choices === 'View all departments') {
+        viewDepartment();
       }
-    })
-    .catch(err => {
-        console.log(err);
+
+      if (choices === 'View all roles') {
+        viewRoles();
+      }
+
+      if (choices === 'View all employees') {
+        viewEmployees();
+      }
+
+      if (choices === 'Add a department') {
+        addDepartment();
+      }
+
+      if (choices === 'Add a role') {
+        addRole();
+      }
+
+      if (choices === 'Add an employee') {
+        addEmployee();
+      }
+
+      if (choices === 'Update an employee role') {
+        updateEmployee();
+      };
     });
-}; // end inquirer
+};
 
-viewDepartment = () => {
-
+const viewDepartment = () => {
+    console.log('Showing all departments...\n');
+    const sql = `SELECT department.id AS id, department.name AS department FROM department`; 
+  
+    connection.query(sql, (err, rows) => {
+      if (err) throw err;
+      console.table(rows);
+      startQuestions();
+    });
 }; // end viewDepartment function
 
 viewRoles = () => {
-
+    console.log('Showing all roles...\n');
+    const sql = `SELECT roles.id, roles.title, department.name AS department 
+                FROM roles 
+                INNER JOIN department ON roles.department_id = department.id`; 
+  
+    connection.query(sql, (err, rows) => {
+      if (err) throw err;
+      console.table(rows);
+      startQuestions();
+    });
 }; // end viewRoles function
 
-viewEmployee = () => {
+viewEmployees = () => {
+    console.log('Showing all employees...\n');
+    const sql = `SELECT employee.id, 
+                    employee.first_name, 
+                    employee.last_name, 
+                    roles.title, 
+                    department.name AS department,
+                    roles.salary, 
+                    CONCAT (manager.first_name, " ", manager.last_name) AS manager
+                FROM employee
+                    LEFT JOIN roles ON employee.role_id = roles.id
+                    LEFT JOIN department ON roles.department_id = department.id
+                    LEFT JOIN employee manager ON employee.manager_id = manager.id`;
 
+    connection.query(sql, (err, rows) => {
+      if (err) throw (err);
+      console.table(rows);
+      startQuestions();
+    });
 }; // End viewEmployee function
 
 addDepartment = () => {
